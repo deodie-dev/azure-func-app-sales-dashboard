75e61aeb1aefe38ce39bf71d51e5c05a17001853
Deodie <deodie.dev@gmail.com>
2025-08-25 09:39:24 +0800

    Update CRON

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ from main import get_deal_data
 app = func.FunctionApp()
 
 # @app.timer_trigger(schedule="0 */15 * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=True) 
-@app.timer_trigger(schedule="0 0 22-23,0-7 * * 1-5", arg_name="myTimer", run_on_startup=True, use_monitor=False) # Set to TRUE
+@app.timer_trigger(schedule="0 */30 22-23,0-7 * * 1-5", arg_name="myTimer", run_on_startup=False, use_monitor=True) # Set to TRUE
 def retrieve_active_campaign_data(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
